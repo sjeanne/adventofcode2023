71e0dbd6024e531b4a2c87f808ac45828c240719
loktor <loktor@gmail.com>
2023-12-11 21:29:36 +0100

    Day 9

---
+++ Day9/1.py
@@ -0,0 +1,27 @@
+input_file = open('1.txt', 'r')
+
+add_umbers = []
+
+while True:
+    line = input_file.readline().strip()
+    if not line:
+        break
+    row = list(map(int,line.split()))
+    pyramid = [row]
+    zero_line = False
+    while not zero_line:
+        previous_line = pyramid[-1]
+        new_line = []
+        for index in range(len(previous_line)-1):
+            new_line.append(previous_line[index+1]- previous_line[index])
+        pyramid.append(new_line)
+        zero_line = sum(new_line) == 0
+    
+    last_inc = pyramid[-2][-1]
+    
+    for i in range(len(pyramid)-2):
+        last_inc = pyramid[len(pyramid)-3-i][-1] + last_inc
+        
+    add_umbers.append(last_inc)
+    
+print(sum(add_umbers))
